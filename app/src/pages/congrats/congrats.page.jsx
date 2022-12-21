511b4e3f9b7fa2b552b372df86f23572993681ec
Guilherme <ferreiraga@outlook.com>
2022-12-21 18:28:34 -0300

    - Added MUI and angular router

---
+++ app/src/pages/congrats/congrats.page.jsx
@@ -0,0 +1,6 @@
+
+import './congrats.styles.scss';
+
+export const CongratsPage = () => {
+    return (<h1>CongratsPage Works</h1>)
+}
---
+++ app/src/pages/home/home.page.jsx
@@ -0,0 +1,6 @@
+import './home.styles.scss';
+import Button from '@mui/material/Button';
+
+export const HomePage = () => {
+    return (<h1>HomePage Works <Button>MUI BUTTON</Button></h1>)
+}
