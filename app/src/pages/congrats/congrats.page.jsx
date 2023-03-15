c2c2530cb5d8ed4b415866969bb032d1c9a9f944
Guilherme <ferreiraga@outlook.com>
2023-03-15 13:57:18 -0300

    saving changes

--- app/src/pages/congrats/congrats.page.jsx
+++
@@ -1,6 +0,0 @@
-
-import './congrats.styles.scss';
-
-export const CongratsPage = () => {
-    return (<h1>CongratsPage Works</h1>)
-}
--- app/src/pages/home/home.page.jsx
+++
@@ -1,6 +0,0 @@
-import './home.styles.scss';
-import Button from '@mui/material/Button';
-
-export const HomePage = () => {
-    return (<h1>HomePage Works <Button>MUI BUTTON</Button></h1>)
-}
